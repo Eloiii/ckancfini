c77369026c6c2d61be66916bd28d9a6061c37f66
Eloiii <eloi.charra@gmail.com>
2022-03-13 16:19:41 +0100

    Add minutes and seconds + to local time

--- public/index.js
+++ public/index.js
@@ -1,16 +1,20 @@
 const timeDIV = document.getElementById("time")
 displayRemainingTime()
-setInterval(displayRemainingTime,3600000)
+setInterval(displayRemainingTime, 1000)
 
 function displayRemainingTime() {
-    const today = Date.now()
+    const today = new Date()
     const end = new Date(2024, 6, 1)
 
     const msRemaining = (end - today)
-    const daysRemaining = Math.floor(msRemaining / (24*60*60*1000))
-    const daysms = msRemaining % (24*60*60*1000)
-    const hoursRemaining = Math.floor(daysms / (60*60*1000))
-    timeDIV.textContent = daysRemaining + ' jours et ' + hoursRemaining + ' heures avant le diplôme'
+    const daysRemaining = Math.floor(msRemaining / (24 * 60 * 60 * 1000))
+    const daysms = msRemaining % (24 * 60 * 60 * 1000)
+    const hoursRemaining = Math.floor(daysms / (60 * 60 * 1000))
+    const hoursms = msRemaining % (60*60*1000);
+    const minutesRemaining = Math.floor(hoursms / (60*1000));
+    const minutesms = msRemaining % (60*1000);
+    const secondsRemaining = Math.floor(minutesms / 1000);
+    timeDIV.textContent = daysRemaining + ' jours ' + hoursRemaining + ' heures ' + minutesRemaining + ' minutes et ' + secondsRemaining + ' secondes ' + 'avant le diplôme'
 }
 
 
